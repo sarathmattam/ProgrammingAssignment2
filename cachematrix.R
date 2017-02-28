0ecc172eafa758b613d929005f7608e397c1c5cf
sarathmattam <sarathmattam@gmail.com>
2017-02-28 09:42:18 +0400

    Comitting programming assignment 2 -  First commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The below set of fucntions are written to calculate the inverse of a matrix, cache the results and return 
+## the inverse value from cache during the next iterations, provided there is change in the input matrix
+## If a different matrix is provided as input, the inverse has to be recalculated and the cache need to be updated
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## makeCacheMatrix is for declaring and initializng the cache variables
+makeCacheMatrix <- function() {
+  if(exists("mCopy") == FALSE) {
+    mCopy <<- NULL
+  }
+  
+  if(exists("inv") == FALSE) {
+    inv <<- NULL
+  }
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cacheSolve function is the parent fuction for the inverse calculation
+cacheSolve <- function(x) {
+  # Initializing vFlag vector to 0
+  vFlag <- 0
+  # Verifying whether the cache variables are existing
+  makeCacheMatrix()
+  # Below function identifies whether the matrix provided in the previous iteration is the same as the 
+  # one provided in the current iteration. If not, the cache need to be refreshed and inverse has to be 
+  # computed again and the new inverse has be to cached
+  vFlag <- setM(x)
+  print(vFlag)
+  # Function to calculate the inverse
+  calcInv(x,vFlag)
 }
+
+## Function to identify whether the matrix provided in the previous iteration is the same as the current one
+setM <- function(x) {
+   
+  if(is.null(mCopy)) {
+      mCopy <<- x
+      return(1)
+  }
+  else if(identical(mCopy, x) == FALSE) {
+      mCopy <<- x
+      return(1)
+  }
+  else {
+    return(0)
+  }
+}
+
+## Function to calculate inverse of a matrix
+calcInv <- function(x, flg) {
+  if(!is.null(inv) & flg == 0) {
+    return(inv)
+  }
+  else {
+    inv <<- solve(x)
+    return(inv)
+  }
+}
+
