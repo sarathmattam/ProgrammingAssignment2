ce43a1c446243b2c276ff84bb162f2034e5a95b0
sarathmattam <sarathmattam@gmail.com>
2017-02-28 09:57:55 +0400

    Updated the cachematrix.R with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,6 @@
-## The below set of fucntions are written to calculate the inverse of a matrix, cache the results and return 
-## the inverse value from cache during the next iterations, provided there is change in the input matrix
-## If a different matrix is provided as input, the inverse has to be recalculated and the cache need to be updated
+# The below set of fucntions are written to calculate the inverse of a matrix, cache the results and return the inverse value from cache during the next iterations, provided there is change in the input matrix. If a different matrix is provided as input, the inverse has to be recalculated and the cache need to be updated
 
-## makeCacheMatrix is for declaring and initializng the cache variables
+# makeCacheMatrix is for declaring and initializng the cache variables
 makeCacheMatrix <- function() {
   if(exists("mCopy") == FALSE) {
     mCopy <<- NULL
@@ -14,22 +12,19 @@ makeCacheMatrix <- function() {
 }
 
 
-## cacheSolve function is the parent fuction for the inverse calculation
+# cacheSolve function is the parent fuction for the inverse calculation
 cacheSolve <- function(x) {
   # Initializing vFlag vector to 0
   vFlag <- 0
   # Verifying whether the cache variables are existing
   makeCacheMatrix()
-  # Below function identifies whether the matrix provided in the previous iteration is the same as the 
-  # one provided in the current iteration. If not, the cache need to be refreshed and inverse has to be 
-  # computed again and the new inverse has be to cached
+  # Comparing matrix with the values in cache
   vFlag <- setM(x)
-  print(vFlag)
   # Function to calculate the inverse
   calcInv(x,vFlag)
 }
 
-## Function to identify whether the matrix provided in the previous iteration is the same as the current one
+# Function to identify whether the matrix provided in the previous iteration is the same as the current one
 setM <- function(x) {
    
   if(is.null(mCopy)) {
@@ -45,7 +40,7 @@ setM <- function(x) {
   }
 }
 
-## Function to calculate inverse of a matrix
+# Function to calculate inverse of a matrix
 calcInv <- function(x, flg) {
   if(!is.null(inv) & flg == 0) {
     return(inv)
